5ad9edd286196fd91ca40c93273e7f2775ce3a88
Celine Sene <celine.sene@gmail.com>
2014-11-18 12:05:37 +0100

    First commit

---
+++ js/ttc.js
@@ -0,0 +1,14 @@
+ $(document).ready(function(){
+    $("#sticky-top").sticky({topSpacing:0});
+	//$("#sidebar").sticky({ topSpacing: 72 });
+
+$('.bxslider').bxSlider({
+  adaptiveHeight: true,
+  mode: 'fade',
+  auto: true,
+  autoControls: true,
+  controls:true,
+  autoHover : true
+});
+
+  });
